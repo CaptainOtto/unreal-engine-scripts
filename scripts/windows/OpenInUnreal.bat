03deebb42058a063a0bd0d0b6888a8ff31d46dbc
CapnJohn <jottossonta@gmail.com>
2022-07-22 13:20:17 +0200

    Update OpenInUnreal.bat

--- scripts/windows/OpenInUnreal.bat
+++ scripts/windows/OpenInUnreal.bat
@@ -1,13 +1,15 @@
 
 @echo off
 
+set PROJECT=ProjectA
 set ROOTDIR=%~dp0
+
 :: Path from current script to root of your game, location of uproject etc.
 set PATHFROMSCRIPTTOGAMEFOLDER=\..\..\Game\
 set ROOTDIR=%ROOTDIR:~0,-1%%PATHFROMSCRIPTTOGAMEFOLDER%
 
 :: Project Name of your project.
-set PROJECT=ProjectA
+
 set PROJECT_DIR=%ROOTDIR%
 set UPROJECT_PATH=%PROJECT_DIR%%PROJECT%.uproject
 
