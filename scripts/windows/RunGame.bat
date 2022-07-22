ab70d28981699ad055dea9c03b1d58729aa83dbf
CapnJohn <jottossonta@gmail.com>
2022-07-22 13:49:20 +0200

    Update RunGame.bat

--- scripts/windows/RunGame.bat
+++ scripts/windows/RunGame.bat
@@ -4,7 +4,10 @@ set PROJECT=ProjectA
 set WINDOWX=1280
 set WINDOWY=720
 set ROOTDIR=%~dp0
-set ROOTDIR=%ROOTDIR:~0,-1%\..\..\Game\
+
+:: Path from current script to root of your game, location of uproject etc.
+set PATHFROMSCRIPTTOGAMEFOLDER=\..\..\Game\
+set ROOTDIR=%ROOTDIR:~0,-1%%PATHFROMSCRIPTTOGAMEFOLDER%
 
 set PROJECT_DIR=%ROOTDIR%
 set GAME_EXE_PATH=%PROJECT_DIR%\Binaries\Win64\%PROJECT%.exe
