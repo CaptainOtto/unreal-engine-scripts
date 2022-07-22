b6c1e50a07f6fece46b68c394a30e58e582782e9
CapnJohn <jottossonta@gmail.com>
2022-07-22 13:20:03 +0200

    Create CookContent.bat

---
+++ scripts/windows/CookContent.bat
@@ -0,0 +1,20 @@
+@echo off
+
+:: Project Name of your project.
+set PROJECT=ProjectA
+set UE_VERS=UE_5.0
+set ROOTDIR=%~dp0
+set PROJECT_DIR=%ROOTDIR%
+
+:: Path from current script to root of your game, location of uproject etc.
+set PATHFROMSCRIPTTOGAMEFOLDER=\..\..\Game\
+set ROOTDIR=%ROOTDIR:~0,-1%%PATHFROMSCRIPTTOGAMEFOLDER%
+
+set UPROJECT_PATH=%PROJECT_DIR%\%PROJECT%.uproject
+
+set UE_DIR=D:\%UE_VERS%
+set EDITOR_EXE=%UE_DIR%\Engine\Binaries\Win64\UnrealEditor-Cmd.exe
+
+CALL "%EDITOR_EXE%" "%UPROJECT_PATH%" -run-cook -targetplatform-windowsNoEditor
+
+pause
